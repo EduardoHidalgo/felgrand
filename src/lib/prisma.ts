af361ab434a11fec1fc4b707a8b480bfb1e8e947
Eduardo Hidalgo <eduardohidalgo.dev@gmail.com>
2024-09-03 18:15:58 -0500

    Switch turso to neon

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -8,16 +6,9 @@ import { createClient } from "@libsql/client";
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-const libsql = createClient({
-  url: String(process.env.TURSO_DATABASE_URL),
-  authToken: process.env.TURSO_AUTH_TOKEN,
-});
-
-const adapter = new PrismaLibSQL(libsql);
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
