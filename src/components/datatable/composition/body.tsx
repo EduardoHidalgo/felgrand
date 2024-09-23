03c5d9a89b4e4ccb22a29a2b43f624979a9c7905
Eduardo Hidalgo <eduardohidalgo.dev@gmail.com>
2024-09-22 19:27:34 -0500

    Removed components

--- src/components/datatable/composition/body.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from "react";
-
-export interface TableBodyProps {
-  children: ReactNode;
-}
-
-export const TableBody: FC<TableBodyProps> = ({ children }) => {
-  return <tbody className="divide-y divide-gray-200">{children}</tbody>;
-};
--- src/components/loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-export interface LoaderProps {}
-
-export const Loader: FC<LoaderProps> = ({}) => {
-  return (
-    <div className="h-8 w-8 animate-spin rounded-full border-[3px] border-solid border-orange-500 border-t-transparent" />
-  );
-};
